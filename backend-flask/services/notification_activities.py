d7657582f96f34e5cc11036d3700612836e38280
rei yoki <751081818@qq.com>
2023-12-12 05:17:21 +0000

    minor fix notifications page and api

---
+++ backend-flask/services/notification_activities.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta, timezone
+
+class NotificationActivities:
+  def run():
+    now = datetime.now(timezone.utc).astimezone()
+    results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'Marlon Leng',
+      'message': 'Marlon try to find new Job!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 10,
+      'replies_count': 12,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Worf',
+        'message': 'This post has no honor at all!',
+        'likes_count': 0,
+        'replies_count': 0,
+        'reposts_count': 0,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    }
+    ]
+    return results 
